d70ae8781d8a3dbf1f300a0cd81c4cf955664e39
jeannillehici <hicianoj@northeastern.edu>
2021-09-30 20:20:27 -0400

    Created Icon for app

--- app/src/main/java/com/example/numad21fa_jeannillehiciano/MainActivity.java
+++ app/src/main/java/com/example/numad21fa_jeannillehiciano/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.numad21fa_jeannillehiciano;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
