9afdd4c4eba2ac125e798e43ea6005f643f0e0c6
jeannillehici <hicianoj@northeastern.edu>
2021-10-03 16:01:51 -0400

    edited onClick & created buttons grid activity

--- app/src/main/java/com/example/numad21fa_jeannillehiciano/MainActivity.java
+++ app/src/main/java/com/example/numad21fa_jeannillehiciano/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.numad21fa_jeannillehiciano;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,17 +11,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        sets contentLayouts specified in what is passed eg. activity_main xml file
         setContentView(R.layout.activity_main);
     }
 
     /** Called when the user touches About Me button */
 
-    public void toastNameInfo(View view) {
-//        Make a standard toast that just contains text.
-        Toast toast = Toast.makeText(this, "Jeannille Hiciano, hiciano.j@northeastern.edu", Toast.LENGTH_LONG);
-        toast.show();
+//    public void toastNameInfo(View view) {
+//        Toast toast = Toast.makeText(this, "Jeannille Hiciano, hiciano.j@northeastern.edu", Toast.LENGTH_LONG);
+//        toast.show();
+//    }
 
-//        TextView txtView = findViewById(R.id.textView)
+    public void onClick(View view){
+//        case for each view (whatever being clicked on) being attached
+
+        switch (view.getId()){
+            case R.id.aboutButton:
+                Toast.makeText(getApplicationContext(), "Jeannille H, hiciano.j", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.clickyButton:
+                Toast.makeText(getApplicationContext(), "Pressed clicky", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 
 
---
+++ app/src/main/java/com/example/numad21fa_jeannillehiciano/grid_buttons_activity.java
@@ -0,0 +1,14 @@
+package com.example.numad21fa_jeannillehiciano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class grid_buttons_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_buttons);
+    }
+}
