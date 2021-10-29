5589d69efffa612bd797adef0be8da1e29015c1d
jeannillehici <hicianoj@northeastern.edu>
2021-10-29 01:10:40 -0400

    created webViewActivity and webServiceActivity

--- app/src/main/java/com/example/numad21fa_jeannillehiciano/WebViewActivity.java
+++ app/src/main/java/com/example/numad21fa_jeannillehiciano/WebViewActivity.java
@@ -3,15 +3,13 @@ package com.example.numad21fa_jeannillehiciano;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-//activity handles the user's input (from WebView), makes HTTP request
-
-public class WebService extends AppCompatActivity {
+//Once response is received after request has been made (via WebServiceActivity), the results
+//are rendered in this webView for the user
+public class WebViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web_service);
+        setContentView(R.layout.activity_web_view);
     }
-
-
 }
