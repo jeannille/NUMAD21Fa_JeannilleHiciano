f6137ca17efce437a36752aa99297041253d26ca
jeannillehici <hicianoj@northeastern.edu>
2021-10-29 01:22:50 -0400

    added comments and request INTERNET permission for app

--- app/src/main/java/com/example/numad21fa_jeannillehiciano/WebViewActivity.java
+++ app/src/main/java/com/example/numad21fa_jeannillehiciano/WebViewActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 //Once response is received after request has been made (via WebServiceActivity), the results
 //are rendered in this webView for the user
+
 public class WebViewActivity extends AppCompatActivity {
+//    Android's WebView object just renders a web page
+//    https://developer.android.com/guide/webapps/webview
+//    this class uses a WebView, and this webView allows us to display content from online
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
