272eb93bdffe996d46ff72f7e3895f8374bebbb7
jeannillehici <hicianoj@northeastern.edu>
2021-10-31 22:09:37 -0400

    created network activity

---
+++ app/src/main/java/com/example/numad21fa_jeannillehiciano/NetworkActivity.java
@@ -0,0 +1,21 @@
+package com.example.numad21fa_jeannillehiciano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// https://developer.android.com/training/basics/network-ops/managing.html
+// checks network connection, user preferences
+public class NetworkActivity extends AppCompatActivity {
+
+
+//    BroadcastReceiver
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network);
+    }
+
+
+}
